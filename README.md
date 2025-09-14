658f49994fed9e4f1b4440c21a9d543ad50665b3
Mansook <106676599+Mansook@users.noreply.github.com>
2025-09-14 22:19:38 +0900

    backend setup

---
+++ README.md
@@ -0,0 +1,5 @@
+의존성 설치
+pip install -r requirements.txt
+
+실행방법
+npm run dev
---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from routers import items
+
+load_dotenv()  # .env 파일 로드
+
+API_KEY = os.getenv("API_KEY") #추후 데이터 로드할때 필요한 키
+app = FastAPI()
+
+app.include_router(items.router)
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, FastAPI!"}
