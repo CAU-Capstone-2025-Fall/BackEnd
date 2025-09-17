4f06069a718c48eb72e413b3b3b1b88ed0a506e4
dlawogud000 <jh020826@gmail.com>
2025-09-17 21:40:13 +0900

    test_main -> main

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import os
 
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from db.auto_fetch import start_scheduler
 from routers import items
+from db import crud_api
 
 load_dotenv()  # .env 파일 로드
 
@@ -10,6 +12,8 @@ API_KEY = os.getenv("API_KEY") #추후 데이터 로드할때 필요한 키
 app = FastAPI()
 
 app.include_router(items.router)
+app.include_router(crud_api.router)
+start_scheduler()
 
 
 @app.get("/")
