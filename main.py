7a6b01d2432b37ffbdcb67c9987f8c796d7a4aa9
Mansook <106676599+Mansook@users.noreply.github.com>
2025-10-03 22:05:48 +0900

    fixed

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from db import crud_api
 from db.auto_fetch import start_scheduler
 from dotenv import load_dotenv
 from fastapi import FastAPI
-from routers import chat, items, img_edit, parse, login
+from routers import chat, img_edit, items, login, parse
 
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
@@ -17,9 +17,9 @@ app.include_router(img_edit.router)
 app.include_router(parse.router)
 app.include_router(login.router)   # 로그인 라우터 연결
 
-@app.on_event("startup")
-def startup_event():
-    start_scheduler()
+#@app.on_event("startup")
+#def startup_event():
+#    start_scheduler()
 
 @app.get("/")
 def read_root():
